e025e4a722664dc2e804e1cedf1a6ece81c03a7c
takada291 <tkp291@gmail.com>
2026-01-29 20:59:40 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'Breast-diameter-v1.0.0';
+const CACHE_NAME = 'Breast-diameter-v1.1.0';
 const urlsToCache = [
   './',
   'index.html',
@@ -38,3 +38,4 @@ self.addEventListener('fetch', (event) => {
   );
 
 });
+
